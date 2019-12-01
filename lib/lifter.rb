23727986af0feb0d625120b2eba5565f52943abb
Alice Won <alicewon4@gmail.com>
2019-12-01 14:35:42 -0800

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,48 @@
-class Lifter
-  attr_reader :name, :lift_total
 
-  def initialize(name, lift_total)
+class Lifter
+  attr_reader :name, :max_weight
+  attr_accessor :gym
+  @@all = []
+  def initialize(name, max_weight)
     @name = name
-    @lift_total = lift_total
+    @max_weight = max_weight
+    @gym = gym
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships #lifter1.memberships
+    Membership.all.select {|membership|membership.lifter == self}
+  end
+
+  def gyms #lifter1.gyms
+    memberships.map {|membership|membership.gym}
+  end
+
+  def self.avg_lift_total #Lifter.avg_lift_total
+    #look at all Lifter's max_weight, add together
+    #divide by lifter.length
+
+    lifter_sum = Lifter.all.sum {|lifter|lifter.max_weight}
+    lifter_count = Lifter.all.count
+    lifter_sum / lifter_count
+  end
+
+  def total_mem_cost #lifter1.total_mem_cost
+   #Get the total cost of a specific lifter's gym memberships
+   memberships.sum {|membership| membership.cost}
+  end
+
+  def sign_up(gym_new, mem_cost) #lifter1.sign_up(gym3, 236)
+    #Given a gym and a membership cost, sign a specific lifter up for a new gym
+    new_membership = Membership.new(self, gym_new, mem_cost)
+    new_membership.gym = gym_new
+    new_membership.cost = mem_cost
+    new_membership
+
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,35 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+lifter1 = Lifter.new("Alice", 200)
+lifter2 = Lifter.new("Aaron", 400)
+lifter3 = Lifter.new("Alex", 100)
+lifter4 = Lifter.new("Anton", 250)
+lifter5 = Lifter.new("Ashley", 350)
+lifter6 = Lifter.new("Annie", 560)
+
+
+gym1 = Gym.new("Gymmy1")
+gym2 = Gym.new("Gymmy2")
+gym3 = Gym.new("Gymmy3")
+
+
+# lifter1.gym = gym1
+# lifter2.gym = gym2
+# lifter3.gym = gym3
+# lifter4.gym = gym1
+# lifter5.gym = gym2
+# lifter6.gym = gym3
+
+mem1 = Membership.new(lifter1, gym1, 200)
+mem2 = Membership.new(lifter2, gym2, 300)
+mem3 = Membership.new(lifter3, gym3, 100)
+mem4 = Membership.new(lifter4, gym1, 250)
+mem5 = Membership.new(lifter5, gym2, 200)
+mem6 = Membership.new(lifter6, gym3, 300)
+mem7 = Membership.new(lifter1, gym2, 100)
+mem8 = Membership.new(lifter2, gym2, 80)
+
 binding.pry
 
 puts "Gains!"
